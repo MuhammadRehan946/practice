5d4f9d5411ce9537cfb185c06cdcb153b29ce38c
Muhammad Rehan <devopsazurecloud1@gmail.com>
2025-02-06 16:27:36 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Sum:", add(num1, num2))
